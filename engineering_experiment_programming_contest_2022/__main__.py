36fb65d43eb4acba90851952ccf618ecdbab0e16
GoRuGoo <dachanyr8989@gmail.com>
2022-10-10 23:18:04 +0900

    readmeの変更及びサーボを連続呼び出しできるようにした

--- engineering_experiment_programming_contest_2022/__main__.py
+++ engineering_experiment_programming_contest_2022/__main__.py
@@ -1,24 +1,14 @@
+import sys
 
 import cv2
 from deepface import DeepFace
 
-from modules.sound import make_sound
-from modules.useful_servo import UsefulServoClass
-import sys
-sys.path.appen('/usr/local/lib/python3.7/dist-packages')
+from .modules.sound import make_sound
+from .modules.useful_servo import UsefulServoClass
 
 cap = cv2.VideoCapture(0)
-judge_sad_nutral = False
 servo_instance = UsefulServoClass()
 servo_instance.set_gpio(13)
-
-
-
-
-
-
-
-
 
 
 def main():
@@ -29,9 +19,10 @@ def main():
             cv2.imshow("FACE", frame)
             if result_["dominant_emotion"] == "sad":
                 cap.release()
-                servo_instance.move_servo(90, 1.0)
-                servo_instance.move_servo(-90, 1.0)
+                cv2.destroyAllWindows(0)
+                servo_instance.move_many_times_servo()
                 make_sound("suisei_amayakasi_trim.wav")
+                sys.exit()
 
 
 if __name__ == "__main__":
